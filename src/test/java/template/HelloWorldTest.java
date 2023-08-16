9f1f239dc402c0cc5ccdb8a4f7f631a3ddedcb1f
StasyanOi <stan.est.high@gmail.com>
2023-08-16 22:06:00 +0300

    refactorings

--- src/test/java/template/HelloWorldTest.java
+++ src/test/java/template/HelloWorldTest.java
@@ -1,15 +1,14 @@
 package template;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class HelloWorldTest {
 
     @Test
     void testGetHelloWorld() {
         var helloWorld = new HelloWorld();
-        Assertions.assertEquals("Hello world!", helloWorld.get());
+        assertEquals("Hello world!", helloWorld.get());
     }
 }
